6de80e3a3d8025d2dc552fa90bbfac9ca6e427e7
Emrullah BUKE <emrullah.buke@outlook.com>
2020-08-22 08:47:42 +0300

    Setup Base DI * Create NetworkModule.kt * Create ApiService.kt * Create BaseApplication.kt * Add dependencies

---
+++ app/src/main/java/com/example/bunqyapp/BaseApplication.kt
@@ -0,0 +1,23 @@
+package com.example.bunqyapp
+
+import android.app.Application
+import com.example.bunqyapp.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class BaseApplication : Application() {
+    /**
+     * We start koin from here, thus, it has application's life cycle and also context.
+     * We can add as many modules as we want for Koin.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@BaseApplication)
+            modules(listOf(networkModule))
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/bunqyapp/network/ApiService.kt
@@ -0,0 +1,3 @@
+package com.example.bunqyapp.network
+
+interface ApiService
